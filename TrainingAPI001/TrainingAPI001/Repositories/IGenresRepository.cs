64fbd6b8c9a81a782ced83ae7a3bace86039194d
L01-NB-19\L01-NB-19 <wuthichai.chuichaeng>
2025-04-03 10:32:35 +0700

    - Updated: Genre API CRUD

--- TrainingAPI001/TrainingAPI001/Repositories/IGenresRepository.cs
+++ TrainingAPI001/TrainingAPI001/Repositories/IGenresRepository.cs
@@ -7,5 +7,8 @@ namespace TrainingAPI001.Repositories
         Task<int> Create(Genre genre);
         Task<Genre> GetById(int id);
         Task<List<Genre>> GetAll();
+        Task<bool> Exists(int id);
+        Task Update(Genre genre);
+        Task Delete(int id);
     }
 }
